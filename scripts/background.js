815e6d3cf673e6250e10ddcf23e01a6680c2d703
Sangha Lee <sanghalee629@gmail.com>
2024-04-20 11:45:48 +0900

    add random background feature

---
+++ scripts/background.js
@@ -0,0 +1,28 @@
+const images = [
+  "airplane.jpg",
+  "birds.jpg",
+  "clock.jpg",
+  "coffee.jpg",
+  "desk.jpg",
+  "giraffe.jpg",
+  "lamp.jpg",
+  "mountain.jpg",
+  "plant.jpg",
+  "sky.jpg",
+  "stairs.jpg",
+  "tree.jpg",
+  "wall.jpg",
+];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+// option 1: changing background image of body
+
+document.body.style.backgroundImage = `url(./images/${chosenImage})`;
+document.body.style.backgroundSize = "100% 100%";
+
+// option 2: inserting image element
+
+const bgImage = document.createElement("img");
+bgImage.src = `./img/${chosenImage}`;
+// document.body.appendChild(bgImage);
